b610906c8ed783b147a095147eafc773f0fbb6e7
Fabricio <fabricio.jacob@outlook.com>
2020-05-28 23:49:56 -0300

    add

---
+++ src/main/java/com/example/demo/reactive/blockingvsnonblocking/Exemplo1.java
@@ -0,0 +1,44 @@
+package com.example.demo.reactive.blockingvsnonblocking;
+
+import io.reactivex.Observable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+public class Exemplo1 {
+
+    public static void main(String[] args) {
+        Observable<Integer> obs = Observable.range(1, 5);
+
+        //block
+        Double d2 = obs.filter(i -> i % 2 == 0)
+                .doOnNext(System.out::println)
+                .map(Math::sqrt)
+                .delay(100, TimeUnit.MILLISECONDS)
+                .blockingLast();
+
+        System.out.println(d2);
+
+        //non-blocking
+        List<Double> list = new ArrayList<>();
+        obs.filter(i -> i % 2 == 0)
+                .doOnNext(System.out::println)
+                .map(Math::sqrt)
+                .delay(100, TimeUnit.MILLISECONDS)
+                .subscribe(d -> {
+                    if(list.size() == 1) {
+                        list.remove(0);
+                    }
+
+                    list.add(d);
+                });
+
+        try{
+             TimeUnit.MILLISECONDS.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(list);
+    }
+}
---
+++ src/main/java/com/example/demo/reactive/coldhot/Example1.java
@@ -0,0 +1,51 @@
+package com.example.demo.reactive.coldhot;
+
+import io.reactivex.Observable;
+import io.reactivex.observables.ConnectableObservable;
+import io.reactivex.processors.PublishProcessor;
+import io.reactivex.schedulers.Schedulers;
+
+import java.util.concurrent.TimeUnit;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+        new Example1().hotBackPressure();
+    }
+
+    //PublishProcessor extend Flowable, que possui controle de contra pressão
+    private void hotBackPressure() {
+        PublishProcessor<Integer> hot = PublishProcessor.create();
+        hot.onBackpressureDrop(v -> System.out.println("Dropped: " + v)) // caso o número de emissão seja maior que o tempo de vazao, feita pelo processamento,
+                // os dados que não conseguirem serem processados, seram descartados.
+                .observeOn(Schedulers.io(), true)
+                .subscribe(System.out::println, Throwable::printStackTrace);
+
+        for(int i = 0; i < 1_000_000; i ++) {
+            hot.onNext(i);
+        }
+    }
+
+    private void hot() {
+        ConnectableObservable<Long> hot = Observable.interval(10, TimeUnit.MILLISECONDS).publish();
+        hot.connect();
+        hot.subscribe(i -> System.out.println("First: " + i));
+        hot.subscribe(i -> System.out.println("Second: " + i));
+        pauseMs(55L);
+    }
+
+    private void cold() {
+        Observable<Long> cold = Observable.interval(10, TimeUnit.MILLISECONDS);
+        cold.subscribe(i -> System.out.println("First: " + i));
+        cold.subscribe(i -> System.out.println("Second: " + i));
+        pauseMs(55L);
+    }
+
+    void pauseMs(Long ms) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
